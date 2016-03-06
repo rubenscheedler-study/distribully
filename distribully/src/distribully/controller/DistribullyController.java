a5b6b7f1e2bbe895cce707e508ad78cf53158c90
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-06 15:08:58 +0100

    YOMAMMA

--- distribully/src/distribully/controller/DistribullyController.java
+++ distribully/src/distribully/controller/DistribullyController.java
@@ -1,8 +1,10 @@
 package distribully.controller;
 import distribully.view.*;
+import static spark.Spark.*;
 
 public class DistribullyController {
 	public static void main(String[] args) {
 		new DistribullyWindow();
+		new YoMomma();
 	}
 }
