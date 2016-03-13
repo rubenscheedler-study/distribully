0f93108e9c8ea051829097ff1a9b25d34310bc08
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-13 12:24:23 +0100

    Changed to have a central server

--- distribully/src/main/distribully/model/Player.java
+++ distribully/src/main/distribully/model/Player.java
@@ -1,7 +1,14 @@
 package distribully.model;
 
-import java.util.Observable;
-
-public class Player extends Observable {
-
+public class Player {
+	private String name;
+	private String ip;
+	private int port;
+	private boolean available;
+	public Player(String name, String ip, int port){
+		this.name = name;
+		this.ip = ip;
+		this.port = port;
+		available = true;
+	}
 }
