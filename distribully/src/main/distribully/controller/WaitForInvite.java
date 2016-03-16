bd962085802384311b4db792200ba931ed232fc0
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-16 13:06:30 +0100

    Thread are now in da houz

---
+++ distribully/src/main/distribully/controller/WaitForInvite.java
@@ -0,0 +1,76 @@
+package distribully.controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WaitForInvite extends Thread {
+	private int port;
+	private boolean listen = false;
+	public WaitForInvite(int port) {
+		this.port = port;
+		listen = true;
+		this.start();
+	}
+	public void run() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			System.out.println("Server is listening for invites...");
+			while (listen) {
+				Socket clientSocket = serverSocket.accept();
+				new Connection(clientSocket);
+			}
+			serverSocket.close();
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+	public void setListen(boolean listen){
+		this.listen = listen;
+	}
+	
+	class Connection{
+		DataInputStream in;
+		DataOutputStream out;
+		Socket clientSocket;
+		public Connection(Socket aSocket) {
+			try {
+				clientSocket = aSocket;
+				in = new DataInputStream(clientSocket.getInputStream());
+				out = new DataOutputStream(clientSocket.getOutputStream());
+				this.handle();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+		private void handle() {
+			try {
+				String hostName = in.readUTF();
+				System.out.println("client>" + hostName); 
+				
+				
+				//show popUp
+				
+				
+				
+				String reversed = new StringBuffer(hostName).reverse().toString();
+				out.writeUTF(reversed);
+				if(reversed.equals("yes")){
+					listen = false;
+					//notify server of unavailability
+				}
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			} finally {
+				try {
+					clientSocket.close();
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				}
+			}
+		}
+	}
+}
---
+++ distribully/src/main/distribully/controller/WaitForStart.java
@@ -0,0 +1,69 @@
+package distribully.controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import distribully.controller.WaitForInvite.Connection;
+
+public class WaitForStart extends Thread {
+	private int port;
+	private boolean listen = false;
+	public WaitForStart(int port) {
+		this.port = port;
+		listen = true;
+		this.start();
+	}
+	public void run() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			System.out.println("Server is listening for queue name...");
+			while (listen) {
+				Socket clientSocket = serverSocket.accept();
+				new Connection(clientSocket);
+			}
+			serverSocket.close();
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+	public void setListen(boolean listen){
+		this.listen = listen;
+	}
+	
+	class Connection{
+		DataInputStream in;
+		DataOutputStream out;
+		Socket clientSocket;
+		public Connection(Socket aSocket) {
+			try {
+				clientSocket = aSocket;
+				in = new DataInputStream(clientSocket.getInputStream());
+				out = new DataOutputStream(clientSocket.getOutputStream());
+				this.handle();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+		private void handle() {
+			try {
+				String queueName = in.readUTF();
+				System.out.println("client>" + queueName);
+				String reversed = new StringBuffer(queueName).reverse().toString();
+				out.writeUTF(reversed);
+				listen = false;
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			} finally {
+				try {
+					clientSocket.close();
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				}
+			}
+		}
+	}
+}
