17af61d05f083ccb3e3a7839dcc8612d19db386a
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-19 13:17:47 +0100

    Bettah Sevvah

--- DistribullyServer/src/server/PlayerServerController.java
+++ DistribullyServer/src/server/PlayerServerController.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
