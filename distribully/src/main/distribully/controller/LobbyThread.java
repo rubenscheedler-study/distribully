c3087edfeedee9254b3cf98bc1d380ee1a75ec8a
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-19 13:10:05 +0100

    LobbyThread

---
+++ distribully/src/main/distribully/controller/LobbyThread.java
@@ -0,0 +1,41 @@
+package distribully.controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+import javax.swing.JOptionPane;
+
+import distribully.controller.WaitForInvite.Connection;
+import distribully.model.DistribullyModel;
+
+public class LobbyThread extends Thread {
+	private boolean inLobby = false;
+	ServerSocket serverSocket;
+	DistribullyModel model;
+
+	public LobbyThread(DistribullyModel model) {
+		this.model = model;
+		inLobby = true;
+		this.start();
+	}
+	public void run() {
+		System.out.println("Starting lobby...");
+		while (inLobby) {
+			//TODO: Get and update game-PlayersList and view
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println("Lobbythread interrupted during sleep!");
+				e.printStackTrace();
+			}			
+		}
+		System.out.println("No longer in lobby.");
+	}
+	public void setInLobby(boolean inLobby){
+		this.inLobby = inLobby;
+	}
+}
