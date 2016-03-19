f545843a7caab680cd3e4e89ddf7404709fe04b2
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-19 13:52:37 +0100

    Now with less exeptions

--- DistribullyServer/src/server/PlayerServerController.java
+++ DistribullyServer/src/server/PlayerServerController.java
@@ -19,24 +19,24 @@ public class PlayerServerController {
 	Gson gson;
 	HashMap<String, ArrayList<Player>> players;
 	JsonParser parser;
-	
+
 	public PlayerServerController(){
 		this.init();
 	}
-	
+
 	private void init() {
 		players = new HashMap<String, ArrayList<Player>>();
 		gson = new Gson();
 		parser = new JsonParser();
 		get("/game/:name", "application/json" , (request, response) -> getPlayerList(request, response)); 
-		
+
 		delete("/game/:name", "application/json", (request, response) -> removeFromPlayerList(request, response));
 		delete("/endGame/:name", "application/json", (request, response) -> deletePlayerList(request, response));
 
 		post("/game/:name", "application/json", (request, response) -> addToPlayerList(request, response));
 		post("/createGame/:name", "application/json", (request, response) -> createPlayerList(request, response));
 	}
-	
+
 	private JsonElement getPlayerList(Request request, Response response) {
 		String gameName = request.params(":name");
 		JsonObject returnObject = new JsonObject();
@@ -45,9 +45,15 @@ public class PlayerServerController {
 			returnObject.addProperty("Message", "Game name is missing.");
 			return returnObject;
 		}
-		System.out.println("Userlist requested");
-		response.status(200);
-		return parser.parse(gson.toJson(players.get(gameName))).getAsJsonArray();
+		System.out.println("Playerlist requested");
+		if(players.containsKey(gameName)){
+			response.status(200);
+			return parser.parse(gson.toJson(players.get(gameName))).getAsJsonArray();
+		}else{
+			response.status(403);
+			returnObject.addProperty("Message", "Game does not exist.");
+			return returnObject;
+		}
 	}
 
 	private JsonObject addToPlayerList(Request request, Response response){
@@ -58,23 +64,25 @@ public class PlayerServerController {
 			returnObject.addProperty("Message", "Game name is missing.");
 			return returnObject;
 		}
+		
+		if(!players.containsKey(gameName)){
+			response.status(403);
+			returnObject.addProperty("Message", "Game does not exist");
+			return returnObject;
+		}
+		
 		String playerJson = request.queryParams("player");
 		JsonElement je = parser.parse(playerJson);
 		JsonObject playerObject = je.getAsJsonObject();
 		Player player = gson.fromJson(playerObject, Player.class);
-		
-		if(!players.containsKey(gameName)){
-			ArrayList<Player> list = new ArrayList<Player>();
-			players.put(gameName, list);
-		}
-		
+
 		players.get(gameName).add(player);
 		response.status(201);
 		System.out.println("User " + player.getName()+ " added to game: " + gameName);
 		returnObject.addProperty("Message", player.getName() + " has been added");
 		return returnObject;
 	}
-	
+
 	private JsonObject createPlayerList(Request request, Response response){
 		String gameName = request.params(":name");
 		JsonObject returnObject = new JsonObject();
@@ -87,14 +95,15 @@ public class PlayerServerController {
 		JsonElement je = parser.parse(playerJson);
 		JsonObject playerObject = je.getAsJsonObject();
 		Player player = gson.fromJson(playerObject, Player.class);
-		
+
 		ArrayList<Player> list = new ArrayList<Player>();
 		list.add(player);
 		players.put(gameName, list);
+		System.out.println("Game created");
 		response.status(201);
 		return returnObject;
 	}
-	
+
 	private JsonObject removeFromPlayerList(Request request, Response response){
 		String gameName = request.params(":name");
 		JsonObject returnObject = new JsonObject();
@@ -105,19 +114,19 @@ public class PlayerServerController {
 		}
 		String playerString = request.queryParams("playerName");
 		if(!players.containsKey(gameName)){
-			response.status(400);
-			returnObject.addProperty("Message", "GameName is not in list.");
+			response.status(403);
+			returnObject.addProperty("Message", "Game does not exist.");
 			return returnObject;
 		}
-		
+
 		if(players.get(gameName).stream().anyMatch(x -> x.getName().equals(playerString))){
 			players.get(gameName).removeIf(p -> p.getName().equals(playerString));
 		}else{
-			response.status(400);
+			response.status(403);
 			returnObject.addProperty("Message", "PlayerName is not in list.");
 			return returnObject;
 		}
-		
+
 		if(players.get(gameName).size() == 0){
 			players.remove(gameName);
 			response.status(200);
@@ -129,7 +138,7 @@ public class PlayerServerController {
 		returnObject.addProperty("Message", "Player "+ playerString +" was removed.");
 		return returnObject;
 	}
-	
+
 	private JsonObject deletePlayerList(Request request, Response response){
 		String gameName = request.params(":name");
 		JsonObject returnObject = new JsonObject();
@@ -138,14 +147,9 @@ public class PlayerServerController {
 			returnObject.addProperty("Message", "Game name is missing.");
 			return returnObject;
 		}
-		if(!players.containsKey(gameName)){
-			response.status(200);
-			returnObject.addProperty("Message", "Game is already gone.");
-			return returnObject;
-		} else{
-			players.remove(gameName);
-			returnObject.addProperty("Message", "Game is now gone.");
-			return returnObject;
-		}
+		players.remove(gameName);
+		response.status(200);
+		returnObject.addProperty("Message", "Game is now gone.");
+		return returnObject;
 	}
 }
