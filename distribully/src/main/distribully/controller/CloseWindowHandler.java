3bebd345cbbf52199d126f9b96ff2bd69a25edcf
rubenscheedler <rubenscheedler@gmail.com>
2016-03-20 12:23:33 +0100

    created rule package

--- distribully/src/main/distribully/controller/CloseWindowHandler.java
+++ distribully/src/main/distribully/controller/CloseWindowHandler.java
@@ -18,6 +18,8 @@ public class CloseWindowHandler extends WindowAdapter {
 		model.getOnlinePlayerList().deleteFromServer(model.getNickname());
 		if(model.getGAME_STATE() == GameState.IN_LOBBY){
 			model.getGamePlayerList().deleteFromGame(model.getNickname(),model.getCurrentHostName());
+		} else if (model.getGAME_STATE() == GameState.INVITING_USERS) {
+			model.getGamePlayerList().deleteGameList(model.getNickname());//=current host name
 		}
         System.out.println("Closed game");
         e.getWindow().dispose();
--- distribully/src/main/distribully/view/SetUpGameButton.java
+++ distribully/src/main/distribully/view/SetUpGameButton.java
@@ -4,9 +4,12 @@ package distribully.view;
 
 import javax.swing.JMenuItem;
 
+import distribully.controller.GameState;
 import distribully.controller.SetUpGameHandler;
+import distribully.model.IObservable;
+import distribully.model.IObserver;
 
-public class SetUpGameButton extends JMenuItem{
+public class SetUpGameButton extends JMenuItem implements IObserver{
 
 	private static final long serialVersionUID = -3732106117892923335L;
 
@@ -14,11 +17,16 @@ public class SetUpGameButton extends JMenuItem{
 	
 	public SetUpGameButton(DistribullyWindow container) {
 		this.container = container;
-		
+		container.getModel().addObserver(this);
 		this.setText("Set up game");
 		
 		this.addActionListener(new SetUpGameHandler(container));
 		
+	}
+
+	@Override
+	public void update(IObservable observable) {
+		this.setEnabled(container.getModel().getGAME_STATE() == GameState.NOT_PLAYING);
 	}
 
 }
