4b12a444eb7f15ca5413d69a561950cd0f4129ee
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-03-21 13:34:51 +0100

    Subclassed consumer

--- distribully/src/main/distribully/controller/GameConsumerThread.java
+++ distribully/src/main/distribully/controller/GameConsumerThread.java
@@ -7,7 +7,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.AMQP.Queue.DeclareOk;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -15,7 +14,6 @@ import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.GetResponse;
 import com.rabbitmq.client.ShutdownSignalException;
 
 import distribully.model.DistribullyModel;
@@ -26,18 +24,12 @@ public class GameConsumerThread extends Thread{
 	DistribullyModel model;
 	String queueName;
 	Channel channel;
-	JsonParser parser;
-	Gson gson;
 	boolean playing;
 	public GameConsumerThread(DistribullyModel model){
 		this.model = model;
 		new ClientListUpdateHandler(model); //Ensure the playerList is up to date
-		ConnectionFactory factory = new ConnectionFactory();  
 		String hostName = model.getCurrentHostName();
 		Player host = model.getOnlinePlayerList().getPlayerByNickname(hostName);
-		factory.setHost(host.getIp());
-		parser = new JsonParser();
-		gson = new Gson();
 		playing = true;
 		try {
 			initPlayerExchange(host);
@@ -69,42 +61,50 @@ public class GameConsumerThread extends Thread{
 			queueName = channel.queueDeclare(model.getNickname(), false, false, false, null).getQueue();
 			channel.exchangeDeclare(playerName, "fanout");
 			channel.queueBind(queueName, playerName, "");
-			Consumer consumer = new DefaultConsumer(channel) {
-			      @Override
-			      public void handleDelivery(String consumerTag, Envelope envelope,
-			                                 AMQP.BasicProperties properties, byte[] body) throws IOException {
-			    	  switch(envelope.getRoutingKey()){
-						case "Start":
-							System.out.println("Game is starting!");
-							if(DistribullyController.lobbyThread != null){
-								DistribullyController.lobbyThread.setInLobby(false);
-							}
-							new ClientListUpdateHandler(model);
-							model.getGamePlayerList().getPlayers().forEach(player -> initPlayerExchange(player));
-							model.setGAME_STATE(GameState.SETTING_RULES);
-							
-							break;
-						case "Leave":
-							JsonElement jeLeave = parser.parse(new String(body));
-							String playerNameLeave = jeLeave.getAsJsonObject().get("playerName").toString();
-							model.getGamePlayerList().getPlayers().removeIf(player -> player.getName().equals(playerNameLeave));
-							System.out.println(playerNameLeave + " left");
-							break;
-						case "Rules":
-							JsonElement jeRule = parser.parse(new String(body));
-							String playerNameRule = jeRule.getAsJsonObject().get("playerName").toString();
-							System.out.println("Rules from  "+ playerNameRule + " received");
-							break;
-						case "PlayCard":
-							break;
-						}
-			      }
-			    };
-			    channel.basicConsume(queueName, true, consumer);
+			Consumer consumer = new MessageConsumer(channel, model);
+			channel.basicConsume(queueName, true, consumer);
 			System.out.println("host connected: " + playerName);
 		} catch (IOException | TimeoutException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	class MessageConsumer extends DefaultConsumer{
+		JsonParser parser;
+		public MessageConsumer(Channel channel, DistribullyModel model) {
+			super(channel);
+			parser = new JsonParser();
+		}
+
+		@Override
+	    public void handleDelivery(String consumerTag, Envelope envelope,
+	                               AMQP.BasicProperties properties, byte[] body) throws IOException {
+	  	  switch(envelope.getRoutingKey()){
+				case "Start":
+					System.out.println("Game is starting!");
+					if(DistribullyController.lobbyThread != null){
+						DistribullyController.lobbyThread.setInLobby(false);
+					}
+					new ClientListUpdateHandler(model);
+					model.getGamePlayerList().getPlayers().forEach(player -> initPlayerExchange(player));
+					model.setGAME_STATE(GameState.SETTING_RULES);
+					
+					break;
+				case "Leave":
+					JsonElement jeLeave = parser.parse(new String(body));
+					String playerNameLeave = jeLeave.getAsJsonObject().get("playerName").toString();
+					model.getGamePlayerList().getPlayers().removeIf(player -> player.getName().equals(playerNameLeave));
+					System.out.println(playerNameLeave + " left");
+					break;
+				case "Rules":
+					JsonElement jeRule = parser.parse(new String(body));
+					String playerNameRule = jeRule.getAsJsonObject().get("playerName").toString();
+					System.out.println("Rules from  "+ playerNameRule + " received");
+					break;
+				case "PlayCard":
+					break;
+				}
+	    }
 	}
 }
